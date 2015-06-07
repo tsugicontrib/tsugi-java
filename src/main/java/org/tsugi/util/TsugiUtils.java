de898e9b37d5e096bdb4dcb35da358814ed6ab74
Charles Severance <csev@umich.edu>
2015-06-07 12:04:17 -0400

    Add a dump properties

--- src/main/java/org/tsugi/util/TsugiUtils.java
+++ src/main/java/org/tsugi/util/TsugiUtils.java
@@ -1,6 +1,8 @@
 package org.tsugi.util;
 
 import java.util.Properties;
+import java.util.Enumeration;
+import java.lang.StringBuffer;
 
 import java.io.InputStream;
 import java.io.IOException;
@@ -18,19 +20,35 @@ public class TsugiUtils {
         Properties prop = new Properties();
         InputStream in = TsugiUtils.class.getResourceAsStream(filename);
         if ( in == null ) {
-            log.error("Properties file not found: "+filename);
+            log.debug("Properties file not found: "+filename);
             return null;
         } else {
             try {
                 prop.load(in);
                 in.close();
-                log.error("Loaded "+prop.size()+" properties from: "+filename);
+                log.debug("Loaded "+prop.size()+" properties from: "+filename);
                 return prop;
             } catch (IOException ex) {
                 log.warn("Failed loading properties from: "+filename,ex);
                 return null;
             }
         }
+    }
+
+    public static String dumpProperties(Properties p)
+    {
+        StringBuffer sb = new StringBuffer();
+
+        Enumeration keys = p.keys();
+        while (keys.hasMoreElements()) {
+            String key = (String)keys.nextElement();
+            String value = (String)p.get(key);
+            if ( sb.length() > 0 ) sb.append("\n");
+            sb.append(key);
+            sb.append(": ");
+            sb.append(value);
+        }
+        return sb.toString();
     }
 
 }
