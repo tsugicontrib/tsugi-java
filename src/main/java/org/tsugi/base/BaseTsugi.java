046d6fd3f620e3b598e33adf1292838b8648253d
Charles Severance <csev@umich.edu>
2015-06-08 15:17:30 -0400

    Tweak

--- src/main/java/org/tsugi/base/BaseTsugi.java
+++ src/main/java/org/tsugi/base/BaseTsugi.java
@@ -2,8 +2,8 @@ package org.tsugi.base;
 
 import org.tsugi.Tsugi;
 import org.tsugi.Launch;
-
 import org.tsugi.util.TsugiUtils;
+import org.tsugi.util.TsugiLTIUtils;
 
 import java.util.Properties;
 import java.sql.DriverManager;
@@ -16,6 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log; 
 import org.apache.commons.logging.LogFactory;
 
+import org.imsglobal.basiclti.BasicLTIUtil;
+
+/*
+ ** Implement the code that is common across implementations here. 
+ */
 public abstract class BaseTsugi implements Tsugi 
 {
 
@@ -33,61 +38,29 @@ public abstract class BaseTsugi implements Tsugi
      */
     public Launch getLaunch(HttpServletRequest req, HttpServletResponse res)
     {
-        return null;
+        Properties props = TsugiLTIUtils.getParameterProperties(req);
+        return getLaunch(req, props, res);
     }
 
    /**
-     * Get a tsugi-suitable connection
+     * Get the launch information for the current session with only properties
      */
-    public Connection getConnection()
+    public Launch getLaunch(Properties props)
     {
-        String jdbc = System.getProperty("tsugi.datasource.url");
-        String username = System.getProperty("tsugi.datasource.username");
-        String password = System.getProperty("tsugi.datasource.password");
-        String className = System.getProperty("tsugi.datasource.driverClassName");
-
-        if ( jdbc == null | username == null || password == null || className == null )
-        {
-            Properties props = TsugiUtils.loadProperties("/tsugi.properties");
-            if ( props != null ) {
-                jdbc = props.getProperty("tsugi.datasource.url");
-                username = props.getProperty("tsugi.datasource.username");
-                password = props.getProperty("tsugi.datasource.password");
-                className = props.getProperty("tsugi.datasource.driverClassName");
-            }
-        }
-
-        if ( jdbc == null | username == null || password == null || className == null )
-        {
-            log.error("Please add to system properties or /tsugi.properties in classpath");
-            log.error("tsugi.datasource.url="+jdbc);
-            log.error("tsugi.datasource.username="+jdbc);
-            log.error("tsugi.datasource.password=*****");
-            log.error("tsugi.datasource.driverClassName="+className);
-            throw new java.lang.RuntimeException("Missing essential Tsugi JDBC properties:");
-        } 
-
-        try {
-            Class.forName(className);
-        } catch (ClassNotFoundException e) {
-            log.error("Missing JDBC Driver: "+className);
-            throw new java.lang.RuntimeException("Missing JDBC Driver: "+className);
-        }
-
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(jdbc, username, password);
-    
-        } catch (SQLException e) {
-            log.error("Your database server may be down.  Or if it is up your database is missing or inaccessible.");
-            log.error("Install the PHP version of Tsugi from www.tsugi.org and use that tool to provision an empty tsugi database.");
-            throw new java.lang.RuntimeException("Database server is down or tsugi database is missing");
-        }
-
-System.out.println("Connection="+connection);
-        return connection;
-
+        return getLaunch(null, props, null);
     }
+
+   /**
+     * This should be private
+     */
+    private Launch getLaunch(HttpServletRequest req, Properties props, HttpServletResponse res)
+    {
+        if ( ! TsugiLTIUtils.isRequest(props) ) {
+            return null;
+        }
+        return null;
+    }
+
 
     public String customGet(String varname, String def)
     {
