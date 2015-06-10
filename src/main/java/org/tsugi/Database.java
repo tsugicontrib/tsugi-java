00be0aab6e0e55095f93cf6566a7148a132551b0
Charles Severance <csev@umich.edu>
2015-06-10 11:27:41 -0400

    Evolving the Database implementation.

--- src/main/java/org/tsugi/Database.java
+++ src/main/java/org/tsugi/Database.java
@@ -10,40 +10,6 @@ import java.sql.SQLException;
 
 /**
  * This is a set of database convienence methods
- *
- * The PDOX class adds a number of non-trivial convienence methods
- * to the underlying PHP PDO class.   These methods combine several
- * PDO calls into a single call for common patterns and add far more
- * extensive error checking and simpler error handling.
- *
- * The primary value is in the queryReturnError() function which
- * combines prepare() and execute() as well as adding in extensive
- * error checking.
- * It turns out that to properly check all of the return values
- * and possible errors which can happen using prepare() and execute()
- * is really challenging and not even obvious from the PDO documentation.
- * So we have collected all that wisdom into this method and then use
- * it throughout Tsugi.
- *
- * The rest of the methods are convienence methods to combine common
- * multi-step operations into a single call to make tool code more readable.
- *
- * While this seems to be bending over backwards, It makes the calling
- * code very succinct as follows:
- *
- *     $stmt = $PDOX->queryDie(
- *         "INSERT INTO .... ",
- *         array('SHA' => $userSHA, ... )
- *     );
- *     if ( $stmt->success) $profile_id = $PDOX->lastInsertId();
- *
- * Whilst many of these methods seem focused on calling the die() function,
- * the only time that die() is called is when there is an SQL syntax error.
- * Not finding a record is non-fatal.  In general SQL syntax errors only
- * happen during development (if you are doing it right) so you might as
- * well die() if there is an SQL syntax error as it most likely indicates
- * a coding bug rather than a runtime user error or missing data problem.
- *
  */
 public interface Database {
 
@@ -56,7 +22,7 @@ public interface Database {
      * @return \ResultSet  This is either the real ResultSet from the query call
      * or an SQLErrror is thrown.
      */
-    public Object queryReturnError(String sql, String[] arr, boolean error_log)
+    public ResultSet queryReturnError(String sql, List<String> arr, boolean error_log)
         throws SQLException;
 
     /**
@@ -67,7 +33,17 @@ public interface Database {
      * @return \ResultSet  This is either the real PDOStatement from the prepare() call
      * or the error is logged and a RuntimeException is thrown.
      */
-    public ResultSet queryDie(String sql, String[] arr);
+    public ResultSet queryDie(String sql, List<String> arr);
+
+    /**
+     * Prepare and execute an SQL insert query and return the new primary key.
+     *
+     * @param sql The SQL to execute in a string.  If the SQL is badly formed this function will die.
+     * @param arr An optional array of the substitition values if needed by the query
+     * @return Long This is the new primary key of the inserted row if the insert was successful.
+     * If anything goes wrong the errors are logged and a runtime exception is thrown.
+     */
+    public Long insertDie(String sql, List<String> arr);
 
     /**
      * Prepare and execute an SQL query and retrieve a single row.
@@ -77,7 +53,7 @@ public interface Database {
      * @return Properties This is either the row that was returned or null if no row was returned.
      * If anything goes wrong the errors are logged and a runtime exception is thrown.
      */
-    public Properties rowDie(String sql, String[] arr);
+    public Properties rowDie(String sql, List<String> arr);
 
     /**
      * Prepare and execute an SQL query and retrieve all the rows as an array
@@ -96,5 +72,5 @@ public interface Database {
      * @return List<Propertiesi> This is either the rows that was returned or null if no rows were returned.
      * If anything goes wrong the errors are logged and a runtime exception is thrown.
      */
-    public List<Properties> allRowsDie(String sql, String[] arr);
+    public List<Properties> allRowsDie(String sql, List<String> arr);
 }
--- src/main/java/org/tsugi/base/BaseDatabase.java
+++ src/main/java/org/tsugi/base/BaseDatabase.java
@@ -7,11 +7,20 @@ import java.util.List;
 import org.tsugi.Database;
 
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
+import org.apache.commons.lang3.StringUtils;
 
-public class BaseDatabase implements Database{
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class BaseDatabase implements Database {
+
+    private Log log = LogFactory.getLog(BaseDatabase.class);
 
     Connection c = null;
     String prefix = null;
@@ -34,10 +43,43 @@ public class BaseDatabase implements Database{
      * @return \ResultSet  This is either the real ResultSet from the query call
      * or an SQLErrror is thrown.
      */
-    public ResultSet queryReturnError(String sql, String[] arr, boolean error_log)
+    public ResultSet queryReturnError(String sql, List<String> arr, boolean error_log)
         throws SQLException
     {
-        return null;
+        sql = setPrefix(sql);
+        log.trace(sql);
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            boolean inserting = StringUtils.trim(sql).toLowerCase().startsWith("insert");
+
+            if ( inserting ) {
+                stmt = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            } else {
+                stmt = c.prepareStatement(sql);
+            }
+
+            if ( arr != null ) for ( int i=0; i < arr.size(); i++) {
+                stmt.setString(i+1, arr.get(i));
+            }
+
+            if ( inserting ) {
+                stmt.executeUpdate();
+                rs = stmt.getGeneratedKeys();
+            } else {
+                rs = stmt.executeQuery();
+            }
+            stmt.close();
+            return rs;
+        // If the closes generate throws, we let them happen
+        // http://stackoverflow.com/questions/321418/where-to-close-java-preparedstatements-and-resultsets
+        } catch(SQLException ex) {
+            if ( stmt != null ) stmt.close();
+            if ( rs != null ) rs.close();
+            throw ex;
+        }
     }
 
     /**
@@ -48,9 +90,40 @@ public class BaseDatabase implements Database{
      * @return \ResultSet  This is either the real PDOStatement from the prepare() call
      * or the error is logged and a RuntimeException is thrown.
      */
-    public ResultSet queryDie(String sql, String[] arr)
+    public ResultSet queryDie(String sql, List<String> arr)
     {
-        return null;
+        try {
+            ResultSet rs = queryReturnError(sql, arr, true);
+            return rs;
+        } catch ( SQLException e ) {
+            log.error("Failed SQL: "+sql);
+            throw new RuntimeException("Failed SQL: "+sql);
+        }
+    }
+
+    /**
+     * Prepare and execute an SQL insert query and return the new primary key.
+     *
+     * @param sql The SQL to execute in a string.  If the SQL is badly formed this function will die.
+     * @param arr An optional array of the substitition values if needed by the query
+     * @return Long This is the new primary key of the inserted row if the insert was successful.
+     * If anything goes wrong the errors are logged and a runtime exception is thrown.
+     */
+    public Long insertDie(String sql, List<String> arr)
+    {
+        ResultSet rs = queryDie(sql, arr);
+        try {
+            if (rs.next()) {
+                Long retval = rs.getLong(1);
+                System.out.println("Inserted new key="+retval);
+                rs.close();
+                return retval;
+            } else {
+                throw new RuntimeException("Insert failed: "+sql);
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("RecordSet close failed:"+sql);
+        }
     }
 
     /**
@@ -61,9 +134,21 @@ public class BaseDatabase implements Database{
      * @return Properties This is either the row that was returned or null if no row was returned.
      * If anything goes wrong the errors are logged and a runtime exception is thrown.
      */
-    public Properties rowDie(String sql, String[] arr)
+    public Properties rowDie(String sql, List<String> arr)
     {
-        return null;
+        ResultSet rs = queryDie(sql, arr);
+        try {
+            if (rs.next()) {
+                Properties retval =  resultsetToProperties(rs);
+                rs.close();
+                return retval;
+            } else {
+                rs.close();
+                return new Properties();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException("RecordSet close failed:"+sql);
+        }
     }
 
     /**
@@ -83,8 +168,38 @@ public class BaseDatabase implements Database{
      * @return List<Propertiesi> This is either the rows that was returned or null if no rows were returned.
      * If anything goes wrong the errors are logged and a runtime exception is thrown.
      */
-    public List<Properties> allRowsDie(String sql, String[] arr)
+    public List<Properties> allRowsDie(String sql, List<String> arr)
     {
         return null;
+    }
+
+
+    /*
+     ** Scan a result set and return a Properties object with entries for each column.
+     */
+    public static Properties resultsetToProperties(ResultSet rs)
+        throws SQLException
+    {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int count = rsmd.getColumnCount();
+        Properties props = new Properties();
+        for ( int i=1; i<=count; i++ ) {
+            String key = rsmd.getColumnLabel(i);
+            String value = rs.getString(i);
+            if ( value == null ) value = "";
+            props.setProperty(key,value);
+        }
+        return props;
+    }
+
+    /*
+     ** Fix the prefix {p} inside of a TSUGI SQL query
+     */
+    public String setPrefix(String sql)
+    {
+        if ( prefix == null ) {
+            return sql.replace("{p}", "");
+        }
+        return sql.replace("{p}", prefix);
     }
 }
