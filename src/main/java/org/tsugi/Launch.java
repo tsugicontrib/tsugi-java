999fc1f139161192725cfde909233752e29935f7
Omkar Sunkersett <omkarsunkersett@Omkars-MacBook-Pro.local>
2018-03-10 19:06:53 -0500

    Added multiple @return statements

--- src/main/java/org/tsugi/Launch.java
+++ src/main/java/org/tsugi/Launch.java
@@ -15,67 +15,80 @@ public interface Launch {
 
     /**
      * Get the HttpRequest associated with the launch.
+     * @return The HttpRequest associated with the launch
      */
     public HttpServletRequest getRequest();
 
     /**
      * Get the HttpResponse associated with the launch.
+     * @return The HttpResponse associated with the launch
      */
     public HttpServletResponse getResponse();
 
     /**
      * Get the HttpSession associated with the launch.
+     * @return The HttpSession associated with the launch
      */
     public HttpSession getSession();
 
     /**
      * Get the User associated with the launch.
+     * @return The User associated with the launch 
      */
     public User getUser();
 
     /**
      * Get the Context associated with the launch.
+     * @return The Context associated with the launch 
      */
     public Context getContext();
 
     /**
      * Get the Link associated with the launch.
+     * @return The Link associated with the launch
      */
     public Link getLink();
 
     /**
      * Get the Result associated with the launch.
+     * @return The Result associated with the launch
      */
     public Result getResult();
 
     /**
      * Get the Service associated with the launch.
+     * @return The Service associated with the launch
      */
     public Service getService();
 
     /**
      * Return the database connection used by Tsugi.
+     * @return The database connection
      */
     public Connection getConnection();
 
     /**
      * Return the database prefix used by Tsugi.
+     * @return The database prefix
      */
     public String getPrefix();
 
     // Deprecated for now
     // /**
     //  * Return the database helper class used by Tsugi.
+    //  * @return The database helper class
     //  */
     // public Database DB();
 
     /**
      * Return the database helper class used by Tsugi.
+     * @return The database helper class
      */
     public Output getOutput();
 
     /**
      * Get the base string from the launch.
+     * @return The base string from the launch
      *
      * @return This is null if it is not the original launch.
      * it is not restored when the launch is restored from 
@@ -85,6 +98,7 @@ public interface Launch {
 
     /**
      * Get the error message if something went wrong with the setup
+     * @return The error message if something went wrong with the setup
      */
     public String getErrorMessage();
 
@@ -101,6 +115,7 @@ public interface Launch {
      *
      * This allows the Tsugi framework to do things like redirect back
      * to itself.
+     * @return True if this request is completely handled
      */
     public boolean isComplete();
 
@@ -109,6 +124,7 @@ public interface Launch {
      *
      * This fails if the LTI Launch was malformed or the session data
      * is corrupted.
+     * @return True if this request is valid
      */
     public boolean isValid();
 
@@ -119,6 +135,7 @@ public interface Launch {
      * point to a URL other than the URL in the request
      * object.  This URL should be used for AJAX calls
      * to dynamic data in JavaScript.
+     * @return A GET URL to the current servlet
      **/
     public String getGetUrl(String path);
 
@@ -128,11 +145,13 @@ public interface Launch {
      * We abstract this in case the framework needs to 
      * point to a URL other than the URL in the request
      * object.
+     * @return A POST URL to the current servlet
      **/
     public String getPostUrl(String path);
 
     /** 
      * Redirect to a path - can be null
+     * @return Redirect to a path (POST Redirect GET)
      **/
     public void postRedirect(String path);
 
@@ -155,11 +174,13 @@ public interface Launch {
      * We abstract this because this might be stored in a
      * CDN for this application. 
      * TODO: Define the property for this
+     * @return A URL to the 'static' folder within this servlet
      **/
     public String getStaticUrl();
 
     /** 
      * Get a URL to a system-wide spinner image
+     * @return A URL to a system-wide spinner image
      **/
     public String getSpinnerUrl();
 
